91de2eded202d7de41c0b8ac4c46915f505bc35c
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2011-08-27 13:12:46 +0200

    added templates shortcuts

--- build/template.js
+++ build/template.js
@@ -1,26 +1,28 @@
-function wru(wru){ // enojy your tests!
+// https://github.com/WebReflection/wru
+function wru(wru){var assert=wru.assert,async=wru.async;
+
+// enojy your tests!
+
+
 
 wru.test([
     {
         name: "it works!",
         test: function () {
+            // sync
             wru.assert(1);
+            
+            // async
+            setTimeout(async(function () { // wru.async
+                assert("called"); // wru.assert
+            }), 500);
         }
     }
 ]);
-        
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
